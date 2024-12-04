4d833e14f928a4ddc71de6d5d9998fdd11bc724b
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-12-04 08:08:00 +0600

    Add CSS with jQuery

--- 03 jQuery/assets/js/main.js
+++ 03 jQuery/assets/js/main.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
     $(".delete-btn").on("click", (function(){
         alert("Are you want to sure delete?"); 
     }));
-});
 
- 
+    //CSS Add 
+    $(".add-css").on("click", (function(){
+        $(".text-css").css("background-color", "yellow",);
+        $(".text-css").css("padding", "18px",);
+        $(".text-css").css("font-size", "24px",);
+    }));
+});
