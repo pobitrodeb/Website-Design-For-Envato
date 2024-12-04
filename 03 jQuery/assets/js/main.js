1496414c599ef6a2487fd6fc42441abbb05f558c
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-12-04 07:48:25 +0600

    Alert Event Added

--- 03 jQuery/assets/js/main.js
+++ 03 jQuery/assets/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-    $("button").on("click",(function(){
+    $(".click-here").on("click",(function(){
         $("p").toggle(1000);
-      }));
-  });
+    }));
+
+    $(".delete-btn").on("click", (function(){
+        alert("Are you want to sure delete?"); 
+    }));
+});
 
  
